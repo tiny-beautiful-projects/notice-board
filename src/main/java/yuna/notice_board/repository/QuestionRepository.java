98dadb6e5873b94c5621a8dfaeb339ddfde1380f
yunajoe <yunajoe@gmail.com>
2026-09-03 18:55:55 +0900

    feat:질문 등록 메서드

---
+++ src/main/java/yuna/notice_board/repository/QuestionRepository.java
@@ -0,0 +1,32 @@
+package yuna.notice_board.repository;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class QuestionRepository {
+
+    private final String url = "";
+    private final String user = "root";
+    private final String password = "istj!@#06207191";
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
+
+   // 글 등록
+    public void insert(String subject, String content){
+        String sql = "INSERT INTO question (subject, content, create_date) VALUES (?, ?, ?)";
+        try{
+            Connection conn = getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, subject);
+            pstmt.setString(2, content);
+            pstmt.executeUpdate();
+            System.out.println(">>> [MySQL] 글 등록 성공!");
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+
+    }
+}
